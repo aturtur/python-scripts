db8cae12493965268df659e02c66726fa7c8213e
aturtur <rautio.arttu@gmail.com>
2016-08-18 19:59:04 +0300

    fixes

--- c4d-multi-passes-to-own-folders.py
+++ c4d-multi-passes-to-own-folders.py
@@ -1,54 +1,36 @@
 # cinema 4d multi-passes to own folders
-# NOTE: ATM does not support object buffers
 import os
 import re
 
-folderPath = r"C:\kansio"
+fp = r"E:\kansio"
 
 items = []
-images = []
 folders = set([])
 
 # scan folder
-for file in os.listdir(folderPath):
+for file in os.listdir(fp):
     items.append(file)
 
+# loop items
 for item in items:
-
     # what folders do we need
     divider = item.rfind('_')
     endPart = item[divider+1:]
     folderName = re.split('(\d.*)',endPart)[0]
     folders.add(folderName)
 
-    # images to array
-    #imageName = re.split('(\d.*)',item)[0]
-    #images.append(imageName)
-
 # make folders
 for item in folders:
-    if not os.path.exists(folderPath+"\\"+item):
-        os.makedirs(folderPath+"\\"+item)
+    if not os.path.exists(fp+"\\"+item):
+        os.makedirs(fp+"\\"+item)
 
 # copy images to folders
 folders = list(folders)
 for i in items:
     for f in folders:
         if folders[folders.index(f)] in i:
-            os.rename(folderPath+"\\"+i, folderPath+"\\"+folders[folders.index(f)]+"\\")
+            oldPath = fp+"\\"+i
+            newPath = fp+"\\"+folders[folders.index(f)]+"\\"+i
+            os.rename(oldPath, newPath)
 
-#
-#print items
-
-# re_pattern = "-*\d+[.]\w+$"
-# .rfind('_')
-# .split('_')[0]
-#newpath = r'C:\Program Files\arbitrary' 
-#if not os.path.exists(newpath):
-    #os.makedirs(newpath)
-
-
-#        for (var k = 0; k < audios.length; k++) {
-#            if (audios[k] === fileformat) {
-#                make_audio = 1;
-#}
+print "done"
